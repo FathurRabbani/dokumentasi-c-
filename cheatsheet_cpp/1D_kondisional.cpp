e54656684f8879e9917df5c7c721b4756023387e
Fathur Rabbani <fathurrabbani88@gmail.com>
2026-05-17 16:06:30 +0700

    updt: menambahkan file kondisional dan perulangan pada cpp

--- cheatsheet_cpp/1D_kondisional.cpp
+++ cheatsheet_cpp/1D_kondisional.cpp
@@ -3,8 +3,13 @@
 int main()
 {
     int nilaiA = 5, nilaiB = 10;
-    std::swap(nilaiA, nilaiB);
+    
+    if(nilaiA == 5){
+        std::swap(nilaiA, nilaiB);
+    }
 
     // Outputs: a=10, b=5
     std::cout << "nilai a=" << nilaiA << ", nilai b=" << nilaiB;
+
+    return 0;
 }
