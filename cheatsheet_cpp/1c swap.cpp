55c450a9ff9616857d8c5d495ed147229c0d4239
Fathur Rabbani <fathurrabbani88@gmail.com>
2026-05-17 15:58:25 +0700

    updt: swap cpp

---
+++ cheatsheet_cpp/1c swap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int nilaiA = 5, nilaiB = 10;
+    std::swap(nilaiA, nilaiB);
+
+    // Outputs: a=10, b=5
+    std::cout << "nilai a=" << nilaiA << ", nilai b=" << nilaiB;
+}
