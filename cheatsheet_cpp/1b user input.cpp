a52100fb91cc8179355e4e33d66ae19f38d1eb67
Fathur Rabbani <fathurrabbani88@gmail.com>
2026-05-17 15:53:32 +0700

    updt: user input cpp

---
+++ cheatsheet_cpp/1b user input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int angka;
+
+    std::cout << "ketik angka: ";
+    std::cin >> angka;
+
+    std::cout << "kamu memasukan angka : " << angka;
+
+    return 0;
+}
